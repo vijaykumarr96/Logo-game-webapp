b91290817d0d0efb9d7e6708dff17314692be5a5
Iamthevk <vijay.vuex@gmail.com>
2023-10-14 23:31:21 +0530

    toggle bookmark feature added

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,34 +1,40 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import styles from "../page.module.css";
 import Bookmark from "./icons/Bookmark";
 import Image from "next/image";
 import Arrow from "./icons/Arrow";
 import ArrowDark from "./icons/ArrowDark";
 import Button from "../ui_components/Button";
-interface Product{
+import FilledBookmark from "./icons/FilledBookmark";
+interface Product {
   brand: string;
   thumbnail: string;
 }
-const ProductCard = ({brand,thumbnail}:Product) => {
+const ProductCard = ({ brand, thumbnail }: Product) => {
+  const [liked, setLiked] = useState(false)
   return (
     <div className={styles.card__container}>
       <div className={styles.card__header}>
         <p className={styles.card__header__text}>{brand}</p>
-        <Bookmark />
+        <div style={{cursor: "pointer"}} onClick={() => setLiked(!liked)}>
+            {liked ? <FilledBookmark/> :<Bookmark />}
+        </div>
       </div>
-      <div className={styles.card__image}>
+      <div className={styles.card__image} >
         <Image
           src={thumbnail}
           alt="card-item"
           width={311}
           height={189}
         />
+         {/* <div className={!liked ? styles.heart__animation : ""} id="heart">❤️</div> */}
       </div>
       <p className={styles.card__item__name}>Headphone-128K</p>
       <Button className={styles.card__button}>
         <div >
-        <p className={styles.card__button__para}>view</p>
-        <ArrowDark className={styles.card__button__img} />
+          <p className={styles.card__button__para}>view</p>
+          <ArrowDark className={styles.card__button__img} />
         </div>
       </Button>
     </div>
